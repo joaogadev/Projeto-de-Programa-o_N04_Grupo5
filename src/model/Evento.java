e78c7b53aa62317224ac2f759ba591f25b7162a9
AbraaoMartins <abraao.neto@souunit.com.br>
2025-10-20 11:26:41 -0300

    Update Evento.java

--- src/model/Evento.java
+++ src/model/Evento.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Evento {
+    private int id;
+    private String titulo;
+    private String descricao;
+
+
+
+  public Evento(){
+  }
+  public Evento(int id, String titulo, String descricao, LocalDate data, LocalTime hora, String local, String categoria, String status, Usuario criador) {
+      this.id = id;
+      this.titulo = titulo;
+      this.descricao = descricao;
+      this.data = data;
+      this.hora = hora;
+      this.local = local;
+      this.categoria = categoria;
+      this.status = status;
+      this.criador = criador;
+    }
+    
 }
