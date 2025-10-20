d2aaa508599b6c1ba9f32b01c465643cbafd9ef9
AbraaoMartins <abraao.neto@souunit.com.br>
2025-10-20 11:19:40 -0300

    Update Usuario.java

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Usuario {
+  private int id;
+  private String nome;
+  private String email;
+  private String senha;
+  private Permissao permissao; // para acessos como ADMIN, VISUALIZADOR E CLIENTE
+
+  public Usuario(){
+  }
+  
+  public Usuario(int id, String nome, String email, String senha, Permissao permissao) {
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+    this.permissao = permissao;
+    }
+  
 }
