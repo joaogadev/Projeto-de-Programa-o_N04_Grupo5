aceeae2a1e79d46e9f06768379bb940f3d29d3a0
João Gabriel <joaogabriel.trindade04@gmail.com>
2025-10-31 17:33:10 -0300

    Criação do calendário pra visualização dos eventos

--- src/ui/AgendaControllerFx.java
+++ src/ui/AgendaControllerFx.java
@@ -1,4 +1,25 @@
 package ui;
 
+import com.toedter.calendar.JCalendar;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import model.Evento;
+import service.EventoService;
 public class AgendaControllerFx {
+    private JFrame frame;
+    JCalendar calendar;
+
+    public AgendaControllerFx() {
+        frame = new JFrame("Agenda de Eventos");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calendar = new JCalendar();
+        frame.add(calendar, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(AgendaControllerFx::new);
+    }
 }
